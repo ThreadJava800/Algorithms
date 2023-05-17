e9c162eae50c6bddd46adb55e1a3754d3495ad29
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-05-17 07:50:49 +0300

    Finished B

--- Contest 3/B.c
+++ Contest 3/B.c
@@ -5,34 +5,10 @@
 #include <math.h>
 #include <string.h>
 
-struct Pair_t {
-    int key;
-    int value;
-};
+typedef short (*CompareFunc_t)(int val1, int val2);
+typedef int (*HashFunc_t)(int value);
 
-typedef struct Pair_t Elem_t;
-typedef bool (*CompareFunc_t)(Elem_t *val1, Elem_t *val2);
-
-const Elem_t POISON          = {};
-const int RESIZE_COEFFICIENT = 2;
-const int MAX_COMMAND_LENGTH = 2 << 8;
-
-struct ListElement_t {
-    Elem_t value;
-    long   previous;
-    long   next;
-};
-
-struct List_t {
-    struct ListElement_t *values;
-    long free;
-
-    long size;
-    long capacity;
-
-    short linearized;
-    short needLinear;
-};
+const int DEFAULT_ARR_SIZE = 990001;
 
 //=========CONTEST STAFF=============
 #define INPUT_CHECK(readFunc, res) {        \
@@ -49,277 +25,269 @@ struct List_t {
 }                                                             \
 //===========================================
 
-void listCtor(struct List_t *list, long listSize, short needLinear);
-void fillElemList(struct ListElement_t *listElems, long capacity);
-long _listInsertPhys(struct List_t *list, Elem_t value, long index);
-long listInsert(struct List_t *list, Elem_t value, long index);
-long listPushBack(struct List_t *list, Elem_t value);
-long listPushFront(struct List_t *list, Elem_t value);
-Elem_t _listRemovePhys(struct List_t *list, long index);
-Elem_t listRemove(struct List_t *list, long index);
-struct ListElement_t *listFind(struct List_t *list, Elem_t *searchElem, CompareFunc_t comparator);
-[[nodiscard]] long logicToPhysics(struct List_t *list, long logicIndex);
+//================LIST STAFF=================
 
-inline __attribute__((always_inline)) Elem_t listGet(struct List_t *list, long index) {
-    ON_ERROR(!list || index > list->capacity || list->size == 0, "Nullptr", POISON);
+struct ListElement_t {
+    int value;
 
-    long physIndex = logicToPhysics(list, index);
-    return list->values[physIndex].value;
-}
+    struct ListElement_t *previous;
+    struct ListElement_t *next;
+};
 
-void listLinearize(struct List_t *list);
-void listResize(struct List_t *list, long newCapacity);
-void listRealloc(struct List_t *list, long newCapacity);
-void poisonList(struct List_t *list, long newCapacity, long oldCapacity);
-void listDtor(struct List_t *list);
-long physicToLogic(struct List_t *list, long start, long phys);
+struct List_t {
+    struct ListElement_t *root;
+    size_t size;
+};
 
-void listCtor(struct List_t *list, long listSize, short needLinear) {\
+void                  listCtor    (struct List_t *list);
+struct ListElement_t *newNode     (int value, struct ListElement_t *prev);
+void                  listPushBack(struct List_t *list, int value);
+short                 listFind    (struct List_t *list, int element, CompareFunc_t comparator);
+void                  listDelete  (struct List_t *list, int value);
+void                  listDtor    (struct List_t *list);
+void listDump(struct List_t *list);
+//=======================HASH MAP=======================
+struct HashMap_t {
+    struct List_t *listArr;
+    int  listCnt;
+
+    HashFunc_t hashFunc;
+};
+
+struct HashMap_t  *hashMapNew   (HashFunc_t hashFunc);
+void               hashMapCtor  (struct HashMap_t *hashMap, HashFunc_t hashFunc);
+void               hashMapDelete(struct HashMap_t *hashMap);
+void               hashMapDtor  (struct HashMap_t *hashMap);
+
+short              comparator(int val1, int val2);
+void               hashMapInsert(struct HashMap_t *hashMap, int key);
+void               hashMapRemove(struct HashMap_t *hashMap, int key);
+short              hashMapSearch(struct HashMap_t *hashMap, int key);
+
+int rotlHash (int value);
+//======================================
+
+void listCtor (struct List_t *list) {
     ON_ERROR(!list, "Nullptr", );
 
-    list->values = (struct ListElement_t*) calloc((size_t) listSize + 1, sizeof(struct ListElement_t));
-    ON_ERROR(!list->values, "Cannot alloc mem", );
-    list->capacity = listSize;
-
-    fillElemList(list->values, listSize);
-
-    list->needLinear = needLinear;
-    list->linearized = 1;
-    list->free       = 1;
-    list->size       = 0;
+    list->size = 0;
+    list->root = NULL;
 }
 
-void fillElemList(struct ListElement_t *listElems, long capacity) {
-    ON_ERROR(!listElems, "Nullptr", );
+struct ListElement_t *newNode(int value, struct ListElement_t *prev) {
+    struct ListElement_t *node = (struct ListElement_t *) calloc(1, sizeof(struct ListElement_t));
+    ON_ERROR(!node, "Nullptr", NULL);
 
-    listElems[0].value    = POISON;
-    listElems[0].previous = 0;
-    listElems[0].next     = 0;
+    node->previous = prev;
+    node->value    = value;
 
-    for (long i = 1; i < capacity + 1; i++) {
-        listElems[i].value    = POISON;
-        listElems[i].previous = -1;
-        
-        if (i != capacity - 1) {
-            listElems[i].next =  i + 1;
-        } else {
-            listElems[i].next = 0;
-        }
-    }
+    return node;
 }
 
-long _listInsertPhys(struct List_t *list, Elem_t value, long index) {
-    ON_ERROR(!list || index > list->capacity + 1, "Nullptr", -1);
-
-    if (list->capacity - list->size <= 2) {
-        long newCapacity = list->capacity * RESIZE_COEFFICIENT;
-        listResize(list, newCapacity);
-    }
-
-    if (index != list->values[0].previous) {
-        list->linearized = 0;
-    }
-
-    long nextFree =  list->values[list->free].next;
-    long pushInd  = list->free;
-
-    list->values[pushInd].value     = value;
-    list->values[pushInd].next      = list->values[index].next;
-    list->values[pushInd].previous  = index;
-
-    list->values[list->values[index].next].previous = list->free;
-    list->values[index]                   .next     = list->free;
- 
-    list->size++;
-    list->free = nextFree;
-
-    return pushInd;
-}
-
-long listInsert(struct List_t *list, Elem_t value, long index) {
-    ON_ERROR(!list || index > list->capacity + 1, "Nullptr", -1);
-
-    long physIndex = logicToPhysics(list, index);
-    return _listInsertPhys(list, value, physIndex);
-}
-
-long listPushBack(struct List_t *list, Elem_t value) {
-    ON_ERROR(!list, "Nullptr", -1);
-
-    long lastInd = list->values[0].previous;
-    return _listInsertPhys(list, value, lastInd);
-}
-
-long listPushFront(struct List_t *list, Elem_t value) {
-    ON_ERROR(!list, "Nullptr", -1);
-
-    return _listInsertPhys(list, value, 0);
-}
-
-Elem_t _listRemovePhys(struct List_t *list, long index) {
-    ON_ERROR(!list || index > list->capacity + 1 || list->size == 0, "Nullptr", POISON);
-
-    if (index != list->values[0].previous) {
-        list->linearized = 0;
-    }
-
-    Elem_t returnValue = list->values[index].value;
-
-    list->values[index].value = POISON;
-
-    list->values[list->values[index].previous].next     = list->values[index].next;
-    list->values[list->values[index].next]    .previous = list->values[index].previous;
-    list->values[index]                       .next     = list->free;
-    list->values[index]                       .next     = list->free;
-    list->values[index]                       .previous = -1;
-
-    list->size--;
-    list->free = index;
-
-    return returnValue;
-}
-
-Elem_t listRemove(struct List_t *list, long index) {
-    ON_ERROR(!list || index > list->capacity + 1 || list->size == 0, "Nullptr", POISON);
-
-    long physIndex = logicToPhysics(list, index);
-    return _listRemovePhys(list, physIndex);
-}
-
-struct ListElement_t *listFind(struct List_t *list, Elem_t *searchElem, CompareFunc_t comparator) {
-    ON_ERROR(!list || !comparator || !searchElem, "Nullptr", NULL);
-
-    for (long i = 0; i < list->size; i++) {
-        if (comparator(searchElem, &(list->values[i].value))) return &(list->values[i]);
-    }
-
-    return NULL;
-}
-
-[[nodiscard]] long logicToPhysics(struct List_t *list, long logicIndex) {
-    ON_ERROR(!list || logicIndex > list->capacity, "Nullptr", -1);
-
-    if (list->size == 0) return 0;
-
-    long pos = list->values[0].next;
-    if (logicIndex == 0) return pos;
-
-    if (list->linearized) return logicIndex;
-
-    for (long i = 0; i < logicIndex; i++) {
-        if (list->values[pos].next == 0) {
-            return pos;
-        }
-        pos =  list->values[pos].next;
-    }
-
-    return pos;
-}
-
-void listLinearize(struct List_t *list) {
+void listPushBack(struct List_t *list, int value) {
     ON_ERROR(!list, "Nullptr", );
 
-    if (list->linearized) return;
-
-    struct ListElement_t *elements = (struct ListElement_t *) calloc((size_t) list->capacity + 1, sizeof(struct ListElement_t));
-    ON_ERROR(!elements, "Nullptr", );
-
-    fillElemList(elements, list->capacity);
-
-    long oldIndex = list->values[0].next;
-    for (long i = 0; i < list->size; i++) {
-        elements[i + 1].value = list->values[oldIndex].value;
-
-        if (i + 1 >= list->size) elements[i + 1].next  = 0;
-        else                     elements[i + 1].next  = i + 2;
-
-        if (i == 0) elements[i + 1].previous = 0;
-        else        elements[i + 1].previous =  i;
-
-        oldIndex =  list->values[oldIndex].next;
-    }
-
-    elements[0].next = 1;
-    elements[0].previous = list->size;
-
-    free(list->values);
-    list->values     = elements;
-    list->linearized = 1;
-    list->free       = list->size + 1;
-}
-
-void listResize(struct List_t *list, long newCapacity) {
-    ON_ERROR(!list, "Nullptr", );
-
-    if (list->needLinear) {
-        listLinearize(list);
-    }
-
-    if (newCapacity < list->capacity) {
-        listRealloc(list, newCapacity);
-        list->values[newCapacity - 1].next = 0;
-
+    if (!list->root) {
+        list->root = newNode(value, NULL);
         return;
     }
 
-    long oldCapacity = list->capacity;
-    listRealloc(list, newCapacity);
-    poisonList (list, newCapacity, oldCapacity);
+    struct ListElement_t *curNode = list->root;
+    while (curNode->next) curNode = curNode->next;
+    
+    curNode->next = newNode(value, curNode);
 }
 
-void listRealloc(struct List_t *list, long newCapacity) {
-    ON_ERROR(!list || !list->values, "Nullptr", );
+short listFind(struct List_t *list, int element, CompareFunc_t comparator) {
+    ON_ERROR(!list, "Nullptr", 0);
 
-    list->values = (struct ListElement_t *) realloc(list->values, (size_t) newCapacity * sizeof(struct ListElement_t));
-    ON_ERROR(!list->values, "Nullptr", );
-
-    list->capacity = newCapacity;
-}
-
-void poisonList(struct List_t *list, long newCapacity, long oldCapacity) {
-    ON_ERROR(!list || !list->values || newCapacity < oldCapacity, "Nullptr", );
-
-    list->values[list->free].next =  oldCapacity;
-
-    for (long i = oldCapacity; i < newCapacity; i++) {
-        list->values[i].value    = POISON;
-        list->values[i].previous = -1;
-
-        if (i == newCapacity - 1) {
-            list->values[i].next = 0;
-        } else {
-            list->values[i].next =  i + 1;
-        }
+    struct ListElement_t *curNode = list->root;
+    while (curNode)
+    {
+        if (comparator(curNode->value, element)) return 1;
+        curNode = curNode->next;
     }
+    
+    return 0;
 }
-
-void listDtor(struct List_t *list) {
+void listDelete(struct List_t *list, int value) {
     ON_ERROR(!list, "Nullptr", );
 
-    if (list->values) {
-        free(list->values);
-    }
+    struct ListElement_t *curNode = list->root;
+    while (curNode)
+    {
+        if (value == curNode->value) {
+            if (!curNode->previous) {
+                list->root = curNode->next;
+                if (curNode->next) curNode->next->previous = NULL;
 
-    list->free       = 0xBEEF;
-    list->size       = 0xBEEF;
-    list->capacity   = 0xBEEF;
-    list->linearized = 1;
+                // free(saveNode);
+                curNode = list->root;
+
+            } else if (!curNode->next) {
+                struct ListElement_t *nextNode = curNode->previous;
+                curNode->previous->next = NULL;
+
+                curNode = nextNode;
+                // free(curNode);
+            } else {
+                struct ListElement_t *nextNode = curNode->previous;
+                curNode->previous->next = curNode->next;
+                curNode->next->previous = curNode->previous;
+
+                curNode = nextNode;
+
+                // free(curNode);
+            }
+        }
+
+        if (curNode && curNode->next) curNode = curNode->next;
+    }
 }
 
-long physicToLogic(struct List_t *list, long start, long phys) {
-    ON_ERROR(!list, "Nullptr", -1);
+void listDtor (struct List_t *list) {
+    ON_ERROR(!list, "Nullptr", );
 
-    long logic = 0;
-    long physInd = start;
-    while (physInd != phys) {
-        logic++;
-        physInd = list->values[physInd].next;
+    while (list->root)
+    {   
+        struct ListElement_t *saveNode = list->root;
+        list->root = list->root->next;
+
+        free(saveNode);
+    }
+    
+    free(list);
+}
+
+void listDump(struct List_t *list) {
+    ON_ERROR(!list, "Nullptr", );
+
+    struct ListElement_t *curNode = list->root;
+    while (curNode)
+    {
+        fprintf(stderr, "%d ", curNode->value);
+        curNode = curNode->next;
+    }
+    fprintf(stderr, "\n");
+}
+
+struct HashMap_t *hashMapNew(HashFunc_t hashFunc) {
+    ON_ERROR(!hashFunc, "Nullptr", NULL);
+
+    struct HashMap_t *hashMap = (struct HashMap_t*) calloc(1, sizeof(struct HashMap_t));
+    ON_ERROR(!hashMap, "Unable to alloc memory", NULL);
+
+    hashMapCtor(hashMap, hashFunc);
+
+    return hashMap;
+}
+
+void hashMapCtor(struct HashMap_t *hashMap, HashFunc_t hashFunc) {
+    ON_ERROR(!hashFunc || !hashMap, "Nullptr",);
+
+    struct List_t *listArr = (struct List_t *) calloc(DEFAULT_ARR_SIZE, sizeof(struct List_t));
+    ON_ERROR(!listArr, "Unable to alloc memory", );
+
+    for (size_t i = 0; i < DEFAULT_ARR_SIZE; i++) {
+        listCtor(&listArr[i]);
     }
 
-    return logic;
+    hashMap->listArr  = listArr;
+    hashMap->hashFunc = hashFunc;
+    hashMap->listCnt  = DEFAULT_ARR_SIZE;  
+}
+
+void hashMapDelete(struct HashMap_t *hashMap) {
+    ON_ERROR(!hashMap, "Nullptr",);
+
+    hashMapDtor(hashMap);
+    free(hashMap);
+}
+
+void hashMapDtor(struct HashMap_t *hashMap) {
+    ON_ERROR(!hashMap, "Nullptr",);
+    ON_ERROR(!hashMap->listArr, "Nullptr",);
+
+    for (int i = 0; i < hashMap->listCnt; i++) {
+        struct List_t listToFree = hashMap->listArr[i];
+
+        listDtor(&listToFree);
+    }
+
+    free(hashMap->listArr);
+}
+
+void hashMapInsert(struct HashMap_t *hashMap, int key) {
+    ON_ERROR(!hashMap, "Nullptr",);
+
+    int hashSum = hashMap->hashFunc(key) % hashMap->listCnt;
+
+    listPushBack(&(hashMap->listArr[hashSum]), key);
+
+    // listDump(&(hashMap->listArr[hashSum]));
+}
+
+void hashMapRemove(struct HashMap_t *hashMap, int key) {
+    ON_ERROR(!hashMap, "Nullptr", );
+
+    int hashSum = hashMap->hashFunc(key) % hashMap->listCnt;
+
+    listDelete(&(hashMap->listArr[hashSum]), key);
+
+    // listDump(&(hashMap->listArr[hashSum]));
+}
+
+short comparator(int val1, int val2) {
+    return val1 == val2;
+}
+
+short hashMapSearch(struct HashMap_t *hashMap, int key) {
+    ON_ERROR(!hashMap || !(hashMap->listArr), "Nullptr", -1);
+
+    int  hashSum    = hashMap->hashFunc(key) % hashMap->listCnt;
+    struct List_t  searchList = hashMap->listArr[hashSum];
+
+    return listFind(&searchList, key, comparator);
+}
+
+int rotlHash  (int value) {
+    return (value << 1) | (value >> 31);
 }
 
 int main() {
+    int n = 0;
+    INPUT_CHECK(scanf("%d", &n), 1);
+
+    struct HashMap_t *hashMap = hashMapNew(rotlHash);
+
+    for (int i = 0; i < n; i++) {
+        char inpBuffer[2];
+        int  value  = 0;
+
+        scanf("%s %d", inpBuffer, &value);
+        // fprintf(stderr, "%d %d\n", symbol, value);
+
+        switch (inpBuffer[0])
+        {
+        case '+':
+            hashMapInsert(hashMap, value);
+            // visualGraph(hashMap->listArr, "Add");
+            break;
+        case '?': {
+            short res = hashMapSearch(hashMap, value);
+
+            if (!res) printf("NO\n");
+            else     printf("YES\n");
+
+            break;
+        }
+        case '-':
+            hashMapRemove(hashMap, value);
+            break;
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
