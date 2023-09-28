4f30d36e47a6e13bf183655b370d729c523cb237
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-09-29 01:10:50 +0300

    Code review

--- Contest 4/J.cpp
+++ Contest 4/J.cpp
@@ -11,9 +11,15 @@
 }                                                             \
 
 
+enum BackTraceMode {
+    FIRST  = 1,
+    SECOND = 2
+};
+
 // backtrace indexing = string index + 1
 // so len(str1) = n - 1 and len(str2) = m - 1
 size_t buildBacktraceArr(std::string& str1, std::string& str2, size_t** backtraceArr, size_t n, size_t m);
+void doBacktrace        (std::string& str1, std::string& str2, size_t** backtraceArr, std::vector<size_t>& meets, BackTraceMode mode);
 
 size_t buildBacktraceArr(std::string& str1, std::string& str2, size_t** backtraceArr, size_t n, size_t m) {
     ON_ERROR(!backtraceArr, "Pointer was nullptr", 0);
@@ -39,6 +45,26 @@ size_t buildBacktraceArr(std::string& str1, std::string& str2, size_t** backtrac
     return backtraceArr[str1Len][str2Len];
 }
 
+void doBacktrace (std::string& str1, std::string& str2, size_t** backtraceArr, std::vector<size_t>& meets, BackTraceMode mode) {
+    ON_ERROR(!backtraceArr, "Pointer was nullptr",);
+
+    size_t i = str1.length();
+    size_t j = str2.length();
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+            if (mode == FIRST) meets.push_back(i - 1);
+            else               meets.push_back(j - 1);
+            i--;
+            j--;
+        }
+        else if (backtraceArr[i][j - 1] > backtraceArr[i - 1][j]) {
+            j--;
+        }
+        else i--;
+    }
+}
+
 int main() {
     std::string str1 = "";
     std::string str2 = "";
@@ -50,11 +76,29 @@ int main() {
     size_t m = str2.length();
 
     size_t** backTrace = new size_t*[n + 1];
+    ON_ERROR(!backTrace, "Unable to alloc memory", 1);
     for (size_t i = 0; i <= n; i++) {
         backTrace[i] = new size_t[m + 1];
+        ON_ERROR(!backTrace[i], "Unable to alloc memory", 1);
     }
 
     std::cout << buildBacktraceArr(str1, str2, backTrace, n + 1, m + 1) << '\n';
+
+    std::vector<size_t> vecStr;
+    doBacktrace(str1, str2, backTrace, vecStr, FIRST);
+
+    for (long i = long(vecStr.size()) - 1; i >= 0; i--) {
+        std::cout << vecStr[size_t(i)] + 1 << ' ';
+    }
+    std::cout << '\n';
+
+    vecStr.clear();
+    doBacktrace(str1, str2, backTrace, vecStr, SECOND);
+    for (long i = long(vecStr.size()) - 1; i >= 0; i--) {
+        std::cout << vecStr[size_t(i)] + 1 << ' ';
+    }
+
+    delete[] backTrace;
 
     return 0;
 }
