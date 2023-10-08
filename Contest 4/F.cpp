fe2feed743e145cd4e23df0f913770aab4259271
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-10-08 23:31:21 +0300

    temp

--- Contest 4/F.cpp
+++ Contest 4/F.cpp
@@ -2,29 +2,32 @@
 #include <vector>
 #include <climits>
 
-static const int INF = INT_MAX;
+static const long long INF = INT_MAX;
 
-int main() {
-    int n = 0, m = 0;
-    std::cin >> m >> n;
+long long getMinMax(long long n, long long m) {
+    if (m == 1 || m == 0) return m;
+    if (n == 1)           return m;
 
-    std::vector<std::vector<int>> attempts(n + 1, std::vector<int>(m + 1, 0));
-    for (int i = 1; i <= n; i++) attempts[i][1] = 1;
-    for (int j = 1; j <= m; j++) attempts[1][j] = j;
-
-
-    for (int i = 2; i <= n; i++) {
-        for (int j = 2; j <= m; j++) {
-            attempts[i][j] = INF;
-
-            for (int h = 1; h <= j; h++) {
-                int temp = std::max(attempts[i - 1][h - 1], attempts[i][j - h]) + 1;
-                attempts[i][j] = std::min(attempts[i][j], temp);
-            }
-        }
+    long long mini = INF;
+    for (long long i = 1; i <= m; i++) {
+        long long temp = std::max(getMinMax(n - 1, i - 1), getMinMax(n, m - i));
+        mini = std::min(temp, mini);
     }
 
-    std::cout << attempts[n][m] - 1 << '\n';
+    return mini + 1;
+}
+
+int main() {
+    long long n = 0, m = 0;
+    std::cin >> m >> n;
+
+    if (m == 0) {
+        std::cout << -1;
+        return 0;
+    }
+
+    // std::vector<std::vector<long long>> attempts(n + 1, std::vector<long long>(m + 1, -1));
+    std::cout << getMinMax(n, m) - 1;
 
     return 0;
 }
