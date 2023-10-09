44568f9aa729b8f9f69648693ba8a19f748012c9
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-10-09 19:10:32 +0300

    Added s

--- Contest 4/S.cpp
+++ Contest 4/S.cpp
@@ -1,50 +1,76 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-    int M = 0, N = 0;
-    std::cin >> M >> N;
+inline bool contains(size_t mask, int check);
+void makeDP (std::vector<std::vector<bool>>* dp, size_t N);
+void makeDP2(std::vector<std::vector<bool>>* dp, std::vector<std::vector<size_t>>* dp2, size_t M);
+size_t getCount(std::vector<std::vector<bool>>* dp, std::vector<std::vector<size_t>>* dp2, size_t M);
 
-    std::vector<std::vector<bool>> dp(1 << M, std::vector<bool>(1 << N, true));
-    for (int i = 1; i < (1 << M); i++) {
-        for (int j = 1; j < (1 << N); j++) {
-            if (!(dp[i - 1][j - 1] && dp[i - 1][j] && dp[i][j - 1]))
-                dp[i][j] = true;
-            else dp[i][j] = false;
+inline bool contains(size_t mask, int check) {
+    return mask & (1 << check);
+}
+
+void makeDP(std::vector<std::vector<bool>>* dp, size_t N) {
+    if (!dp) return;
+
+    for (size_t i = 0; i < dp->size(); i++) {
+        for (size_t j = 0; j < dp->size(); j++) {
+            bool cell = true;
+
+            for (size_t k = 0; k < N - 1; k++) {
+                int checkSum = contains(i, k) + contains(i, k + 1) + contains(j, k) + contains(j, k + 1);
+                
+                cell = !(checkSum == 0 || checkSum == 4);
+                if (!cell) break;
+            }
+
+            (*dp)[i][j] = cell;
         }
     }
+}
 
-    for (int i = 0; i < (1 << M); i++) {
-        for (int j = 0; j < (1 << N); j++) {
-            std::cout << dp[i][j] << ' ';
-        }
-        std::cout << '\n';
-    }
+void makeDP2(std::vector<std::vector<bool>>* dp, std::vector<std::vector<size_t>>* dp2, size_t M) {
+    if (!dp || !dp2) return;
 
-    std::vector<std::vector<int>> a(1 << M, std::vector<int>(1 << N, 0));
-    a[0][0] = 2;
+    for (size_t i = 0; i < dp->size(); i++) (*dp2)[0][i] = 1;
 
-    for (int k = 1; k < N; k++) {
-        for (int i = 0; i < (1 << N); i++) {
-            for (int j = 1; j < (1 << M); j++) {
-                a[k][i] += a[k - 1][j] * dp[j][i] + a[k - 1][j - 1] * dp[j][i] + a[k][j - 1] * dp[j][i];
+    for (size_t i = 1; i < M; i++) {
+        for (size_t j = 0; j < dp->size(); j++) {
+            for (size_t k = 0; k < dp->size(); k++) {
+                if ((*dp)[j][k]) (*dp2)[i][k] += (*dp2)[i - 1][j];
             }
         }
     }
-std::cout << '\n';
-    for (int i = 0; i < (1 << M); i++) {
-        for (int j = 0; j < (1 << N); j++) {
-            std::cout << a[i][j] << ' ';
-        }
-        std::cout << '\n';
+}
+
+size_t getCount(std::vector<std::vector<bool>>* dp, std::vector<std::vector<size_t>>* dp2, size_t M) {
+    if (!dp || !dp2) return 0;
+
+    size_t count = 0;
+
+    for (size_t i = 0; i < dp->size(); i++) count += (*dp2)[M - 1][i];
+
+    return count;
+}
+
+int main() {
+    int N = 0, M = 0;
+    std::cin >> N >> M;
+
+    if (N > M) {
+        int temp = M;
+        M = N;
+        N = temp;
     }
 
-    // int res = 0;
-    // for (int i = 0; i < (1 << M); i++) {
-    //     if () {
+    std::vector<std::vector<bool>>   dp(1 << N, std::vector<bool>(1 << N, true));
+    std::vector<std::vector<size_t>> dp2(M, std::vector<size_t>(1 << N, 0));
+    makeDP (&dp, N);
+    makeDP2(&dp, &dp2, M);
 
-    //     }
-    // }
+    size_t variants = getCount(&dp, &dp2, M);
+
+    std::cout << variants << '\n';
 
     return 0;
 }
