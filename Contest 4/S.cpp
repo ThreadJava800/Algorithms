1af439a890f2d6829b863be9f89f9a44702453a6
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-11-14 23:45:44 +0300

    Replaced cycle with bit masks

--- Contest 4/S.cpp
+++ Contest 4/S.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
-inline bool contains(size_t mask, int check);
-void   makeAreaDP  (std::vector<std::vector<bool>>* areaDP, size_t N);
-void   makeAreaDP2 (std::vector<std::vector<bool>>* areaDP, std::vector<std::vector<size_t>>* areaDP2, size_t M);
-size_t getCount    (std::vector<std::vector<bool>>* areaDP, std::vector<std::vector<size_t>>* areaDP2, size_t M);
+inline bool contains(const size_t mask, const int check);
+void   makeAreaDP   (      std::vector<std::vector<bool>>* areaDP, const size_t N);
+void   makeAreaDP2  (const std::vector<std::vector<bool>>* areaDP,       std::vector<std::vector<size_t>>* areaDP2, const size_t M);
+size_t getCount     (const std::vector<std::vector<bool>>* areaDP, const std::vector<std::vector<size_t>>* areaDP2, const size_t M);
 
-inline bool contains(size_t mask, int check) {
+inline bool contains(const size_t mask, const int check) {
     return mask & (1 << check);
 }
 
-void makeDP(std::vector<std::vector<bool>>* areaDP, size_t N) {
+inline bool checkProfile(size_t profile1, size_t profile2, size_t profileLimit = SSIZE_MAX) {
+    size_t greenMatch  = profile1 & profile2;
+    size_t purpleMatch = (~profile1 & ~profile2) & (profileLimit - 1);
+
+    return ((greenMatch & (greenMatch << 1)) == 0) && ((purpleMatch & (purpleMatch << 1)) == 0);
+}
+
+void makeAreaDP(std::vector<std::vector<bool>>* areaDP, const size_t N) {
     if (!areaDP) return;
 
     for (size_t i = 0; i < areaDP->size(); i++) {
         for (size_t j = 0; j < areaDP->size(); j++) {
-            bool cell = true;
+            // bool cell = true;
 
-            for (size_t k = 0; k < N - 1; k++) {
-                int checkSum = contains(i, k) + contains(i, k + 1) + contains(j, k) + contains(j, k + 1);
+            // for (size_t k = 0; k < N - 1; k++) {
+            //     int checkSum = contains(i, k) + contains(i, k + 1) + contains(j, k) + contains(j, k + 1);
                 
-                cell = !(checkSum == 0 || checkSum == 4);
-                if (!cell) break;
-            }
+            //     cell = !(checkSum == 0 || checkSum == 4);
+            //     if (!cell) break;   
+            // }
 
-            (*areaDP)[i][j] = cell;
+            (*areaDP)[i][j] = checkProfile(i, j, areaDP->size());
         }
     }
 }
 
-void makeDP2(std::vector<std::vector<bool>>* areaDP, std::vector<std::vector<size_t>>* areaDP2, size_t M) {
+void makeAreaDP2(const std::vector<std::vector<bool>>* areaDP, std::vector<std::vector<size_t>>* areaDP2, const size_t M) {
     if (!areaDP || !areaDP2) return;
 
     for (size_t i = 0; i < areaDP->size(); i++) (*areaDP2)[0][i] = 1;
@@ -43,7 +51,7 @@ void makeDP2(std::vector<std::vector<bool>>* areaDP, std::vector<std::vector<siz
     }
 }
 
-size_t getCount(std::vector<std::vector<bool>>* areaDP, std::vector<std::vector<size_t>>* areaDP2, size_t M) {
+size_t getCount(const std::vector<std::vector<bool>>* areaDP, const std::vector<std::vector<size_t>>* areaDP2, const size_t M) {
     if (!areaDP || !areaDP2) return 0;
 
     size_t count = 0;
@@ -53,24 +61,24 @@ size_t getCount(std::vector<std::vector<bool>>* areaDP, std::vector<std::vector<
     return count;
 }
 
+size_t proceedAlgorithm(size_t N, size_t M) {
+    if (N > M) std::swap(N, M);
+
+    std::vector<std::vector<bool>>   areaDP (1 << N, std::vector<bool>(1 << N, true));
+    std::vector<std::vector<size_t>> areaDP2(M, std::vector<size_t>(1 << N, 0));
+    makeAreaDP (&areaDP, N);
+    makeAreaDP2(&areaDP, &areaDP2, M);
+
+    size_t variants = getCount(&areaDP, &areaDP2, M);
+
+    return variants;
+}
+
 int main() {
-    int N = 0, M = 0;
+    size_t N = 0, M = 0;
     std::cin >> N >> M;
 
-    if (N > M) {
-        int temp = M;
-        M = N;
-        N = temp;
-    }
-
-    std::vector<std::vector<bool>>   dp(1 << N, std::vector<bool>(1 << N, true));
-    std::vector<std::vector<size_t>> dp2(M, std::vector<size_t>(1 << N, 0));
-    makeDP (&dp, N);
-    makeDP2(&dp, &dp2, M);
-
-    size_t variants = getCount(&dp, &dp2, M);
-
-    std::cout << variants << '\n';
+    std::cout << proceedAlgorithm(N, M) << '\n';
 
     return 0;
 }
