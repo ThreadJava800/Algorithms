8bac04ee173f6c600ee162f3fba92e9f1d49b291
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-11-14 22:19:14 +0300

    Replace ** with vec

--- Contest 4/J.cpp
+++ Contest 4/J.cpp
@@ -3,13 +3,7 @@
 #include <cmath>
 #include <vector>
 
-#define ON_ERROR(expr, errStr, retVal) {                 \
-    if (expr) {                                           \
-        fprintf(stderr, "FATAL ERROR: %s\n", errStr);      \
-        return retVal;                                      \
-    }                                                        \
-}                                                             \
-
+using lluvec2 = std::vector<std::vector<size_t>>;
 
 enum BackTraceMode {
     FIRST  = 1,
@@ -18,12 +12,10 @@ enum BackTraceMode {
 
 // backtrace indexing = string index + 1
 // so len(str1) = n - 1 and len(str2) = m - 1
-size_t buildBacktraceArr(std::string& str1, std::string& str2, size_t** backtraceArr, size_t n, size_t m);
-void doBacktrace        (std::string& str1, std::string& str2, size_t** backtraceArr, std::vector<size_t>& meets, BackTraceMode mode);
+size_t buildBacktraceArr(std::string& str1, std::string& str2, lluvec2& backtraceArr, size_t n, size_t m);
+void doBacktrace        (std::string& str1, std::string& str2, lluvec2& backtraceArr, std::vector<size_t>& meets, BackTraceMode mode);
 
-size_t buildBacktraceArr(std::string& str1, std::string& str2, size_t** backtraceArr, size_t n, size_t m) {
-    ON_ERROR(!backtraceArr, "Pointer was nullptr", 0);
-
+size_t buildBacktraceArr(std::string& str1, std::string& str2, lluvec2& backtraceArr, size_t n, size_t m) {
     size_t str1Len = n - 1;
     size_t str2Len = m - 1;
 
@@ -45,9 +37,7 @@ size_t buildBacktraceArr(std::string& str1, std::string& str2, size_t** backtrac
     return backtraceArr[str1Len][str2Len];
 }
 
-void doBacktrace (std::string& str1, std::string& str2, size_t** backtraceArr, std::vector<size_t>& meets, BackTraceMode mode) {
-    ON_ERROR(!backtraceArr, "Pointer was nullptr",);
-
+void doBacktrace (std::string& str1, std::string& str2, lluvec2& backtraceArr, std::vector<size_t>& meets, BackTraceMode mode) {
     size_t i = str1.length();
     size_t j = str2.length();
 
@@ -75,11 +65,9 @@ int main() {
     size_t n = str1.length();
     size_t m = str2.length();
 
-    size_t** backTrace = new size_t*[n + 1];
-    ON_ERROR(!backTrace, "Unable to alloc memory", 1);
+    lluvec2 backTrace;
     for (size_t i = 0; i <= n; i++) {
-        backTrace[i] = new size_t[m + 1];
-        ON_ERROR(!backTrace[i], "Unable to alloc memory", 1);
+        backTrace.push_back(std::vector<size_t>(m + 1));
     }
 
     std::cout << buildBacktraceArr(str1, str2, backTrace, n + 1, m + 1) << '\n';
@@ -97,9 +85,6 @@ int main() {
     for (long i = long(vecStr.size()) - 1; i >= 0; i--) {
         std::cout << vecStr[size_t(i)] + 1 << ' ';
     }
-
-    for (size_t i = 0; i <= n; i++) delete[] backTrace[i];
-    delete[] backTrace;
 
     return 0;
 }
