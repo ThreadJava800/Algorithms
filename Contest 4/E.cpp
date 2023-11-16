30a64d533cc141da59c2e2ff3a0ec6ebf3ac8c00
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-11-16 19:10:27 +0300

    Code review #3

--- Contest 4/E.cpp
+++ Contest 4/E.cpp
@@ -5,10 +5,12 @@ static const int PLUS_INF = 1e8;
 
 using ivec2 = std::vector<std::vector<int>>;
 
-void  getInput                (const size_t n, const size_t m, std::vector<int>& coords);
-void  generateDistanceArray   (std::vector<int>& d, const std::vector<int>& coords);
-void  generateDistancesDPArray(std::vector<int>* d, const std::vector<int>& coords,       ivec2& distancesDPArr, const size_t m);
-void  printAnswer             (std::vector<int>& d, const std::vector<int>& coords, const ivec2& distancesDPArr, const size_t m);
+void  getInput                   (const size_t n, const size_t m, std::vector<int>& coords);
+void  generateDistanceArray      (std::vector<int>& d, const std::vector<int>& coords);
+void  generateDistancesDPArray   (std::vector<int>* d, const std::vector<int>& coords,       ivec2& distancesDPArr, const size_t m);
+void  calculateInfluencePoints   (std::vector<int>& d, const std::vector<int>& coords, const ivec2& distancesDPArr, const size_t m);
+void  printInfluencePoints       (const std::vector<int>& d, const std::vector<int>& coords, const size_t m);
+void  findAndPrintInfluencePoints(const size_t n, const size_t m);
 
 void getInput(const size_t n, const size_t m, std::vector<int>& coords) {
     for (size_t i = 0; i < n; i++)
@@ -56,7 +58,7 @@ void generateDistancesDPArray(std::vector<int>* d, const std::vector<int>& coord
     }
 }
 
-void printAnswer(std::vector<int>& d, const std::vector<int>& coords, const ivec2& distancesDPArr, const size_t m) {
+void calculateInfluencePoints(std::vector<int>& d, const std::vector<int>& coords, const ivec2& distancesDPArr, const size_t m) {
     size_t coordsSize = coords.size();
 
     int mini      = PLUS_INF;
@@ -82,16 +84,15 @@ void printAnswer(std::vector<int>& d, const std::vector<int>& coords, const ivec
         rightBase = distancesDPArr[rightBase][k];
         k--;
     }
-    
+}
+
+void printInfluencePoints(const std::vector<int>& d, const std::vector<int>& coords, const size_t m) {
     for (int i = 0; i < m; i++) {
         std::cout << coords[d[i]] << ' ';
     }
 }
 
-int main() {
-    size_t n = 0, m = 0;
-    std::cin >> n >> m;
-
+void findAndPrintInfluencePoints(const size_t n, const size_t m) {
     std::vector<int> coords(n, 0);
     std::vector<int> d(n, 0);
 
@@ -104,7 +105,15 @@ int main() {
 
     generateDistanceArray(d, coords);
     generateDistancesDPArray(&d, coords, distancesDPArr, m);
-    printAnswer(d, coords, distancesDPArr, m);
+    calculateInfluencePoints(d, coords, distancesDPArr, m);
+    printInfluencePoints(d, coords, m);
+}
+
+int main() {
+    size_t n = 0, m = 0;
+    std::cin >> n >> m;
+
+    findAndPrintInfluencePoints(n, m);
 
     return 0;
 }
