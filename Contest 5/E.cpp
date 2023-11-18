f8254996209e53b0e6fbca3f511852ce251d10af
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-11-19 02:50:37 +0300

    Rewrite to classes

--- Contest 5/E.cpp
+++ Contest 5/E.cpp
@@ -5,25 +5,92 @@
 using intDim2 = std::vector<std::vector<std::pair<int, int>>>;
 using pairVec = std::vector<std::pair<int, int>>;
 
+struct Edge {
+    int from   = 0;
+    int to     = 0;
+    int number = 0;
+};
+
+struct GraphTime {
+    int in  = 0;
+    int out = 0;
+};
+
+struct GraphVertex {
+    int vert   = 0;
+    int parent = 0;
+};
+
+class Graph {
+private:
+    int timeCnt;
+
+    std::vector<std::vector<Edge>> nonOrientedEdges;
+    std::vector<GraphTime>         time;
+    std::vector<bool>              used;
+
+    void dfs(const GraphVertex curPoint, std::vector<int>& bridges) {
+        used[curPoint.vert] = true;
+        time[curPoint.vert].in = time[curPoint.vert].out = timeCnt++;
+
+        size_t pathCnt = nonOrientedEdges[curPoint.vert].size();
+        for (size_t i = 0; i < pathCnt; i++) {
+            int edgeEnd = nonOrientedEdges[curPoint.vert][i].to;
+
+            if (edgeEnd == curPoint.parent) continue;  // back edge
+
+            if (used[edgeEnd]) time[curPoint.vert].out 
+                                    = std::min(time[curPoint.vert].out, time[edgeEnd].in);
+            else {
+                dfs({edgeEnd, curPoint.vert}, bridges);
+                time[curPoint.vert].out 
+                                    = std::min(time[curPoint.vert].out, time[edgeEnd].out);
+
+                if (time[edgeEnd].out > time[curPoint.vert].in) {
+                    bridges.push_back(nonOrientedEdges[curPoint.vert][i].number);
+                }
+            }
+        }
+    }
+
+public:
+    explicit Graph(int N) : timeCnt(0) {
+        nonOrientedEdges.resize(N);
+    }
+
+    void addEdge(int v1, int v2, int number) {
+        nonOrientedEdges[v1].push_back({v1, v2, number});
+        nonOrientedEdges[v2].push_back({v2, v1, number});
+    }
+
+    std::vector<int> findBridges() {
+        std::vector<int> bridges;
+
+        size_t vertexCnt = nonOrientedEdges.size();
+
+        used.resize(vertexCnt, false);
+        time.resize(vertexCnt);
+
+        for (int i = 0; i < vertexCnt; i++) {
+            if (!used[i]) dfs({i, -1}, bridges);
+        }
+
+        return bridges;
+    }
+};
+
 //--------------------------------------------------------------------
 
-intDim2*          readArguments(pairVec& startEdges);
-void              dfs(const intDim2& paths, 
-                            pairVec& times, 
-                            int bfsCnt, 
-                      const std::pair<int, int> curPoint, 
-                            std::vector<bool>& used,
-                            std::vector<int>& edge);
-                            
-std::vector<int>* dfsDriver(const intDim2& paths);
-
+Graph readArguments     ();
+void  printBridgesSorted(std::vector<int>& bridges);
+                        
 //--------------------------------------------------------------------
 
-intDim2* readArguments(pairVec& startEdges) {
+Graph readArguments() {
     int N = 0, M = 0;
     std::cin >> N >> M;
 
-    intDim2* graph = new std::vector(N, std::vector<std::pair<int, int>>());
+    Graph graph(N);
 
     for (int i = 0; i < M; i++) {
         int p1 = 0, p2 = 0;
@@ -31,77 +98,28 @@ intDim2* readArguments(pairVec& startEdges) {
 
         p1--; p2--;
 
-        startEdges.push_back({p1, p2});
-
-        (*graph)[p1].push_back({p2, i + 1});
-        (*graph)[p2].push_back({p1, i + 1});
+        graph.addEdge(p1, p2, i + 1);
     }
 
     return graph;
 }
 
-//--------------------------------------------------------------------
+void printBridgesSorted(std::vector<int>& bridges) {
+    size_t edgeCnt = bridges.size();
 
-void dfs(const intDim2& paths, 
-               pairVec& times, 
-               int bfsCnt, 
-         const std::pair<int, int> curPoint, 
-               std::vector<bool>& used, 
-               std::vector<int>& edge) 
-{
-    used[curPoint.first] = true;
-    times[curPoint.first].first = times[curPoint.first].second = bfsCnt++;
+    std::cout << edgeCnt << '\n';
 
-    size_t pathCnt = paths[curPoint.first].size();
-    for (size_t i = 0; i < pathCnt; i++) {
-        int edgeEnd = paths[curPoint.first][i].first;
-
-        if (edgeEnd == curPoint.second) continue;  // back edge
-
-        if (used[edgeEnd]) times[curPoint.first].second 
-                                = std::min(times[curPoint.first].second, times[edgeEnd].first);
-        else {
-            dfs(paths, times, bfsCnt, {edgeEnd, curPoint.first}, used, edge);
-            times[curPoint.first].second 
-                                = std::min(times[curPoint.first].second, times[edgeEnd].second);
-
-            if (times[edgeEnd].second > times[curPoint.first].first) {
-                edge.push_back(paths[curPoint.first][i].second);
-            }
-        }
-    }
-}
-
-std::vector<int>* dfsDriver(const intDim2& paths) {
-    size_t vertexCnt = paths.size();
-
-    std::vector<bool> used  = std::vector(vertexCnt, false);
-    pairVec           times = std::vector(vertexCnt, std::pair<int, int>(0, 0));
-    std::vector<int>* edges = new std::vector<int>();
-
-    for (size_t i = 0; i < vertexCnt; i++) {
-        if (!used[i]) dfs(paths, times, 0, {i, -1}, used, *edges);
-    }
-
-    return edges;
+    std::sort(bridges.begin(), bridges.end());
+    for (int i = 0; i < edgeCnt; i++) std::cout << bridges[i] << '\n';
 }
 
 //--------------------------------------------------------------------
 
 int main() {
-    pairVec           startEdges;
-    intDim2*          paths       = readArguments(startEdges);
-    std::vector<int>* bridgeEdges = dfsDriver(*paths);
+    Graph graph = readArguments();
 
-    size_t pathCnt = startEdges.size();
-    size_t edgeCnt = bridgeEdges->size();
-
-    std::cout << edgeCnt << '\n';
-
-    std::sort(bridgeEdges->begin(), bridgeEdges->end());
-    for (int i = 0; i < edgeCnt; i++) std::cout << (*bridgeEdges)[i] << '\n';
-
-    delete paths; delete bridgeEdges;
+    std::vector<int> bridges = graph.findBridges();
+    printBridgesSorted(bridges);
 
     return 0;
 }
