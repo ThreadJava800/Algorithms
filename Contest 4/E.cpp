ece4f70ea06a191323aa08674e509acee3cebcd1
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-11-18 23:46:14 +0300

    Code review #3

--- Contest 4/E.cpp
+++ Contest 4/E.cpp
@@ -5,31 +5,32 @@ static const int PLUS_INF = 1e8;
 
 using ivec2 = std::vector<std::vector<int>>;
 
-void  getInput                   (const size_t n, const size_t m, std::vector<int>& coords);
-void  generateDistanceArray      (std::vector<int>& d, const std::vector<int>& coords);
-void  generateDistancesDPArray   (std::vector<int>* d, const std::vector<int>& coords,       ivec2& distancesDPArr, const size_t m);
-void  calculateInfluencePoints   (std::vector<int>& d, const std::vector<int>& coords, const ivec2& distancesDPArr, const size_t m);
-void  printInfluencePoints       (const std::vector<int>& d, const std::vector<int>& coords, const size_t m);
-void  findAndPrintInfluencePoints(const size_t n, const size_t m);
+void             getInput                (const size_t n, const size_t m, std::vector<int>& coords);
+void             generateDistanceArray   (std::vector<int>& dist, const std::vector<int>& coords);
+void             generateDistancesDPArray(std::vector<int>* dist, const std::vector<int>& coords,       ivec2& distancesDPArr, const size_t m);
+int              calculateInfluencePoints(std::vector<int>& dist, const std::vector<int>& coords, const ivec2& distancesDPArr, const size_t m);
+void             printVector             (const std::vector<int>& vec);
+std::vector<int> createInfluencePointList(const std::vector<int>& dist, const std::vector<int>& coords, const size_t m);
+std::vector<int> getInfluencePoints      (const size_t n, const size_t m);
 
 void getInput(const size_t n, const size_t m, std::vector<int>& coords) {
     for (size_t i = 0; i < n; i++)
         std::cin >> coords[i];
 }
 
-void generateDistanceArray(std::vector<int>& d, const std::vector<int>& coords) {
+void generateDistanceArray(std::vector<int>& dist, const std::vector<int>& coords) {
     size_t coordsSize = coords.size();
     for (int i = 0; i < coordsSize; i++) {
         int sum = 0;
         for (int j = 0; j <= i - 1; j++) {
             sum += coords[i] - coords[j];
         }
-        d[i] = sum;
+        dist[i] = sum;
     }
 }
 
-void generateDistancesDPArray(std::vector<int>* d, const std::vector<int>& coords, ivec2& distancesDPArr, const size_t m) {
-    if (!d) return;
+void generateDistancesDPArray(std::vector<int>* dist, const std::vector<int>& coords, ivec2& distancesDPArr, const size_t m) {
+    if (!dist) return;
 
     size_t coordsSize = coords.size();
     std::vector<int> kthDistances(coordsSize, 0);
@@ -48,53 +49,63 @@ void generateDistancesDPArray(std::vector<int>* d, const std::vector<int>& coord
                 }
 
                 summa += (r - j - 1) * (coords[j + 1] - coords[j]);
-                if (summa + (*d)[j] < kthDistances[i]) {
-                    kthDistances[i] = summa + (*d)[j];
+                if (summa + (*dist)[j] < kthDistances[i]) {
+                    kthDistances[i] = summa + (*dist)[j];
                     distancesDPArr[i][k] = j;
                 }
             }
         }
-        *d = kthDistances;
+        *dist = kthDistances;
     }
 }
 
-void calculateInfluencePoints(std::vector<int>& d, const std::vector<int>& coords, const ivec2& distancesDPArr, const size_t m) {
+int calculateInfluencePoints(std::vector<int>& dist, const std::vector<int>& coords, const ivec2& distancesDPArr, const size_t m) {
     size_t coordsSize = coords.size();
 
-    int mini      = PLUS_INF;
+    int minSumma  = PLUS_INF;
     int rightBase = 0;
     for (int i = 0; i < coordsSize; i++) {
-        int summa = d[i];
+        int summa = dist[i];
 
         for (int j = i + 1; j < coordsSize; j++) 
             summa += (coords[j] - coords[i]);
 
-        if (summa < mini) {
-            mini = summa;
+        if (summa < minSumma) {
+            minSumma = summa;
             rightBase = i;
         }
     }
 
-    std::cout << mini << '\n';
-
     int k = m - 1;
     while (k >= 0)
     {
-        d[k] = rightBase;
+        dist[k] = rightBase;
         rightBase = distancesDPArr[rightBase][k];
         k--;
     }
+
+    return minSumma;
 }
 
-void printInfluencePoints(const std::vector<int>& d, const std::vector<int>& coords, const size_t m) {
-    for (int i = 0; i < m; i++) {
-        std::cout << coords[d[i]] << ' ';
+void printVector(const std::vector<int>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] << ' ';
     }
 }
 
-void findAndPrintInfluencePoints(const size_t n, const size_t m) {
+std::vector<int> createInfluencePointList(const std::vector<int>& dist, const std::vector<int>& coords, const size_t m) {
+    std::vector<int> influencePoints;
+
+    for (int i = 0; i < m; i++) {
+        influencePoints.push_back(coords[dist[i]]);
+    }
+
+    return influencePoints;
+}
+
+std::vector<int> getInfluencePoints(const size_t n, const size_t m) {
     std::vector<int> coords(n, 0);
-    std::vector<int> d(n, 0);
+    std::vector<int> dist(n, 0);
 
     ivec2 distancesDPArr = std::vector<std::vector<int>>();
     for (size_t i = 0; i < n; i++) {
@@ -103,17 +114,20 @@ void findAndPrintInfluencePoints(const size_t n, const size_t m) {
 
     getInput(n, m, coords);
 
-    generateDistanceArray(d, coords);
-    generateDistancesDPArray(&d, coords, distancesDPArr, m);
-    calculateInfluencePoints(d, coords, distancesDPArr, m);
-    printInfluencePoints(d, coords, m);
+    generateDistanceArray(dist, coords);
+    generateDistancesDPArray(&dist, coords, distancesDPArr, m);
+    int minSumma = calculateInfluencePoints(dist, coords, distancesDPArr, m);
+    std::cout << minSumma << '\n';
+
+    return createInfluencePointList(dist, coords, m);
 }
 
 int main() {
     size_t n = 0, m = 0;
     std::cin >> n >> m;
 
-    findAndPrintInfluencePoints(n, m);
+    auto points = getInfluencePoints(n, m);
+    printVector(points);
 
     return 0;
 }
